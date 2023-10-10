9cd9650410f62f018933a9fb10c5b7a23898cf2c
Coderner <shreyashukla680@gmail.com>
2023-10-10 16:04:17 +0530

    created buttons list

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Button from './Button'
+
+const list = ["All", "Sitcom", "Games", "Food", "Accessories", "Songs", "Poetry", "News", "Spoken Fest", "Aesthetics", "CppNuts",
+"React", "Striver" ];
 
 const ButtonList = () => {
   return (
-    <div>ButtonList</div>
+    <div className='flex'>
+      {list.map((listItem)=>(
+        <Button name={listItem}/>
+      ))}
+    </div>
   )
 }
 
